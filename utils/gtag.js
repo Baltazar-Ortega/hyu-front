108bf259924e0d4ec06ef719234812e4871084ab
Baltazar-Ortega <kalbertog121@gmail.com>
2022-09-01 13:58:08 -0500

    logs removed

--- utils/gtag.js
+++ utils/gtag.js
@@ -1,10 +1,7 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
-console.log("GA_TRACKING_ID: ", GA_TRACKING_ID);
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
-  console.log("GA_TRACKING_ID 2: ", GA_TRACKING_ID);
-
   window.gtag("config", GA_TRACKING_ID, {
     page_path: url,
   });
